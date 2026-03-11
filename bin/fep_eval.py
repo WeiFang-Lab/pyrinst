fix(io): correct unit conversion in load

bin/fep_eval.py
@@ -8,7 +8,7 @@ import numpy as np
 from pyrinst.config.constants import KB
 from pyrinst.io.xyz import load
 from pyrinst.utils.fep import *
-from pyrinst.utils.units import Energy
+from pyrinst.utils.units import EV, Energy
 
 
 def main():
@@ -27,10 +27,10 @@ def main():
         filenames, read_coords=False, energy_pattern=r"energy\s*=\s*['\"]?([-+]?\d*\.?\d+(?:[eE][-+]?\d+)?)['\"]?"
     )
 
-    beads_energies -= input_geom.energy
+    beads_energies = beads_energies * EV - input_geom.energy
     aes = np.average(beads_energies, axis=0)
     bhs = input_geom.harm_energies
-    des = aes * Energy(1, "eV").get("Hartree") - bhs  # eV -> Hartree
+    des = aes - bhs
 
     beta = 1.0 / (input_geom.T * KB)  # 1/Hartree
 

src/pyrinst/io/xyz.py
@@ -10,7 +10,7 @@ from io import TextIOWrapper
 
 import numpy as np
 
-from pyrinst.utils.units import Length
+from pyrinst.utils.units import ANGSTROM, Length
 
 # Configure a logger for the library.
 # The user of the library can then configure the root logger to see these messages.
@@ -336,7 +336,7 @@ def load(filepath: str | list[str], read_coords: bool = True, energy_pattern: st
         count = len(file_coords) if read_coords else (len(file_comments) if file_comments else "unknown")
         print(f"Successfully loaded {count} frame(s) from '{path}'.")
 
-    ret_coords = np.atleast_2d(np.squeeze(np.array(all_file_coords)))
+    ret_coords = np.atleast_2d(np.squeeze(np.array(all_file_coords))) * ANGSTROM
     ret_comments = np.array(all_file_comments, dtype=float if isinstance(energy_pattern, str) else str)
     if is_single_file and energy_pattern:
         ret_comments = ret_comments[0]

src/pyrinst/potentials/mace.py
@@ -48,7 +48,7 @@ class MACE(Potential):
         self.atoms.calc = calculator
         self.symbols = symbols
 
-    def __call__(self, x: NDArray, task: Task = Task.SP) -> tuple[float, None, NDArray | None]:
+    def __call__(self, x: NDArray, task: Task = Task.SP) -> tuple[float, NDArray | None, NDArray | None]:
         arr = np.asarray(x, dtype=float).reshape(-1, 3) * Length(1, "au").get("A")
         self.atoms.set_positions(arr)
         energy_conv = Energy(1.0, "eV").get("Hartree")