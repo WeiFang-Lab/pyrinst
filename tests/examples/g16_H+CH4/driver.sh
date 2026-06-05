361abee94408f2729f89ef6f71161b4cde78ddb5
Ziyan Ye <zyye22@m.fudan.edu.cn>
2026-06-05 17:32:04 +0800

    fix: remove parallel flag from driver script for Gaussian execution

--- tests/examples/g16_H+CH4/driver.sh
+++ tests/examples/g16_H+CH4/driver.sh
@@ -3,4 +3,4 @@
 export PYTHONBUFFERED=1
 export GAUSS_SCRDIR=/tmp/GAU-pyrinst
 
-pyrinst-driver --parallel -P Gaussian --working-dir beads -F header_doublet --runcmd g16
+pyrinst-driver -P Gaussian --working-dir beads -F header_doublet --runcmd g16
